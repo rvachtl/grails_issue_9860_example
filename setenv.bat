a35a92f891de62043def9cb412776b31af25c558
rvachtl <radim.vachtl@gmail.com>
2016-08-23 14:25:27 +0100

    Delete setenv.bat

--- setenv.bat
+++
@@ -1,2 +0,0 @@
-SET JAVA_OPTS=-Dhttps.proxyHost=webproxy.services.adroot.ms -Dhttps.proxyPort=8080 -Dhttp.proxyHost=webproxy -Dhttp.proxyPort=8080
-rem NOTE: you need to add proxy settings into ~/gradle.properties!
